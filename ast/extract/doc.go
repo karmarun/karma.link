44930d1972dfe23fd40ed4f9c6f5d2d59fccda4f
Thomas Wilde <thomas.wilde@dudagroup.com>
2018-07-12 17:05:16 +0200

    go lint suggested these changes

--- ast/extract/doc.go
+++ ast/extract/doc.go
@@ -1,4 +1,4 @@
 // Copyright 2018 karma.run AG. All rights reserved.
 
-// Package ast/extract contains methods to extract type information from ast.Node trees.
+// Package extract contains methods to extract type information from ast.Node trees.
 package extract // import "github.com/karmarun/karma.link/ast/extract"
--- auth/fs/doc.go
+++ auth/fs/doc.go
@@ -1,4 +1,4 @@
 // Copyright 2018 karma.run AG. All rights reserved.
 
-// Package auth/fs implements a filesystem-based auth.Authenticator.
+// Package fs implements a filesystem-based auth.Authenticator.
 package fs // import "github.com/karmarun/karma.link/auth/fs"
